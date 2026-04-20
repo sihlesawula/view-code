b396006ade15c8bc824c76a6dff77a570f69845f
Sihle Sawula <150780503+sihlesawula@users.noreply.github.com>
2026-04-20 12:18:55 +0200

    budget

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const nav = document.getElementById("nav");
 menu.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+const slider = document.getElementById("budgetSlider");
+const valueText = document.getElementById("budgetValue");
+const packageText = document.getElementById("packageText");
+
+// Detect package based on value
+function getPackage(value) {
+  if (value < 3000) {
+    return "Starter";
+  } else if (value < 6500) {
+    return "Professional";
+  } else {
+    return "Premium";
+  }
+}
+
+slider.addEventListener("input", function () {
+  const value = Number(this.value);
+
+  valueText.textContent = "R" + value.toLocaleString();
+  packageText.textContent = getPackage(value);
+});
+
